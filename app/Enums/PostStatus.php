40aa25227dd177a173330d225e4f44b5d60df37b
VU HUY HOANG <v_huyhoang@thk-hd.vn>
2026-01-14 16:03:08 +0700

    test

--- app/Enums/PostStatus.php
+++ app/Enums/PostStatus.php
@@ -7,6 +7,6 @@ enum PostStatus: string
     case Draft = 'draft';
     case Pending = 'pending';
     case Published = 'published';
-	case Test = 'test';
-	case Test2 = 'test2';
+    case Test = 'test';
+    case Test2 = 'test2';
 }
